504569c6f0e31837e28eb05c3f6a198120b12f8e
Emmanuel Kipsang <emmqnuelkipsqng@gmail.com>
2023-11-17 11:34:24 -0800

    banner component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { SubscribeComponent } from './pages/subscribe/subscribe.component';
+import { BannerComponent } from './shared/banner/banner.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    AboutComponent,
+    SubscribeComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
