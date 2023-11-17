9723b9c4bd15c0e090842018a0a911f5747c83f0
Emmanuel Kipsang <emmqnuelkipsqng@gmail.com>
2023-11-17 12:25:49 -0800

    routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './pages/about/about.component';
+import { HomeComponent } from './pages/home/home.component';
+import { SubscribeComponent } from './pages/subscribe/subscribe.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component:HomeComponent},
+  {path: 'about', component:AboutComponent},
+  {path: 'subscribe', component:SubscribeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
